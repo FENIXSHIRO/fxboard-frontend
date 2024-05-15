bdc8b7205b58a77d22c56f9cabd9b60018e2d32f
Иван Тетеев <fenixshiro@gmail.com>
2024-05-15 11:48:43 +0300

    скрыл плейсхолдеры

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -32,9 +32,11 @@
       <h1 class="text-left font-bold">Доступные доски</h1>
       <hr class="my-1 h-px border-t-0 bg-[#ddd]" />
       <div class="flex flex-wrap m-5 ml-0">
+        <!-- 
         <div v-for="n in 3" class="me-3 mb-3 w-[150px] h-[150px] border shadow-md rounded-lg align-middle hover:bg-[#eee]">
           {{n}}
         </div>
+        -->
       </div>
     </div>
   </div>
